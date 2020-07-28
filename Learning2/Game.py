d5d0ed432466889e72ef6639dfe1ae6cf282d713
nedwards3 <nedwards3@ucmerced.edu>
2020-07-28 14:37:42 -0700

    Learning how to make states
    Using chess since there are alot of examples to learn from first

--- Learning2/Game.py
+++ Learning2/Game.py
@@ -1,1 +1,10 @@
-print("2")
+import pygame,sys
+import Constants
+
+def read(mapfile):
+    with open(mapfile,"r")as f:
+        world_map=f.readlines()
+def main():
+    read_map(Constants.mapfile)
+if __name__=="main":
+    main()
