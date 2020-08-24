701e12ca83ef0c306cfca8f1a25a7dc84e5b58ca
nedwards3 <nedwards3@ucmerced.edu>
2020-08-24 15:25:03 -0700

    Adding game states

--- Learning3/ChessEngine.py
+++ Learning3/ChessEngine.py
@@ -1,4 +1,21 @@
 #Valid moves and game state
 import pygame as p
-from ChessEngine import ChessEngine
+from Learning3 import ChessEngine
 Width=Height=512
+Dimension=8 #8by8 board
+SQ_SIZE=Height/Dimension
+Max_Fps=15
+IMAGES={}
+
+def load_Images():
+    pieces = ["wp","wR","wN","wB","wK","wQ","bp","bR",'bN',"bK","bQ"]
+    for unit in pieces:
+        IMAGES[unit]=p.transform.scale(p.image.load("images/"+unit+".png"),(SQ_SIZE,SQ_SIZE))
+
+def main():
+    p.init()
+    screen=p.display.set_mode((Width,Height))
+    clock = p.time.Clock()
+    screen.fill(p.Color("White"))
+    gs=ChessEngine.GameState()
+    print(gs.board)
